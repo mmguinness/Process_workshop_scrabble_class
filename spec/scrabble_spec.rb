fe39d4df2b053a886c0868d49643f1a37cc31c7a
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-05-03 10:55:41 +0100

    edge cases included in testing

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -13,9 +13,29 @@ describe Scrabble do
       expect(scrabble.score).to eq 4
     end
 
-     it 'accepts a word and returns the total value' do
+    it 'accepts a word in lower case letters and returns the total value' do
       scrabble = Scrabble.new("street")
       expect(scrabble.score).to eq 6
+    end
+
+    it 'accepts a word in capital letters and returns the total value' do
+      scrabble = Scrabble.new("OXYPHENBUTAZONE")
+      expect(scrabble.score).to eq 41
+    end
+
+    it 'accepts an empty string and returns the zero' do
+      scrabble = Scrabble.new(" ")
+      expect(scrabble.score).to eq 0
+    end
+
+    it 'accepts nil and returns the zero' do
+      scrabble = Scrabble.new(nil)
+      expect(scrabble.score).to eq 0
+    end
+
+    it 'accepts " \t\n" and returns the zero' do
+      scrabble = Scrabble.new(" \t\n")
+      expect(scrabble.score).to eq 0
     end
   end
 
